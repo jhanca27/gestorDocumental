599c54ac1c6d27166c4a62a5cbe7a0e8add69de8
jhanca <jhancarlohigueracarvajal@gmail.com>
2023-10-30 08:48:09 -0500

    Starting proyect dependencias: jpa, devtools, web, mysql, thymeleaf

---
+++ src/main/java/com/sena/solution/GestorDocumentalApplication.java
@@ -0,0 +1,13 @@
+package com.sena.solution;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestorDocumentalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestorDocumentalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sena/solution/GestorDocumentalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sena.solution;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestorDocumentalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
